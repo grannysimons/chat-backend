5d050c7cd6a39d4505d4e10568a966f40bc145f9
Mariona Roca <mariona.roca@gmail.com>
2018-11-12 17:16:53 +0100

    deploying...

--- app.js
+++ app.js
@@ -29,15 +29,15 @@ app.use(cors());
 //   }
 // }
 
-app.use((req, res, next) => {
-  // res.setHeader('Access-Control-Allow-Origin', process.env.frontend_BaseURL);
-  res.setHeader('Access-Control-Allow-Origin', 'https://txatapp.firebaseapp.com');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.setHeader('Access-Control-Allow-Credentials', true);
+// app.use((req, res, next) => {
+//   // res.setHeader('Access-Control-Allow-Origin', process.env.frontend_BaseURL);
+//   res.setHeader('Access-Control-Allow-Origin', 'https://txatapp.firebaseapp.com');
+//   res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+//   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+//   res.setHeader('Access-Control-Allow-Credentials', true);
 
-  next();
-});
+//   next();
+// });
 
 // sessions handler
 app.use(session({
